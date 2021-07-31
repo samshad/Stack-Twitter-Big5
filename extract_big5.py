e36befffe99914a4d88d6189e8d676aae331ffd6
Samshad <samshad.ru@gmail.com>
2021-07-31 15:35:23 +0600

    big5

---
+++ extract_big5.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import json
+import os
+
+
+files = os.listdir('Data/Personality/')
+
+arr = []
+for file in files:
+    tmp = [file.split('_personality.json')[0]]
+
+    with open('Data/Personality/' + file, 'r') as f:
+        data = json.load(f)
+    
+    for i in data['personality']:
+        # print(i['name'], i['percentile'])
+        tmp.append(i['percentile'])
+    
+    for i in data['values']:
+        # print(i['name'], i['percentile'])
+        tmp.append(i['percentile'])
+    
+    arr.append(tmp)
+
+df = pd.DataFrame(arr, columns=['users', 'Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 
+                                'Emotional range', 'Conservation', 'Openness to change', 'Hedonism', 
+                                'Self-enhancement', 'Self-transcendence'])
+
+df.to_csv('Data/personality_data.csv', index=False)
+
+"""
+Openness
+Conscientiousness
+Extraversion
+Agreeableness
+Emotional range
+Conservation
+Openness to change
+Hedonism
+Self-enhancement
+Self-transcendence
+"""
